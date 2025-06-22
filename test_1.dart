0cad20542aa6ace2e6531634ea9cb9153e95f65b
mysha815 <myshaakter815@gmail.com>
2025-06-22 12:02:04 +0600

    Add files via upload

---
+++ test_1.dart
@@ -0,0 +1,16 @@
+main(){
+  for(int i=1; i<=5; i++){
+    print('mysha $i');
+  }
+
+  int i=5;
+
+  while(i<=10){
+    print('mysha:$i');
+    i++;
+  }
+  List<String> fruits=['mango','apple','lichi'];
+for(String fruit in fruits) {
+  print('$fruit khai na');
+}
+}
